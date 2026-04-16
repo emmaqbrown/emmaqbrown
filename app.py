1c3cd0aa604c19e4d1bf1cbd6dde2498a3b74d63
emma <eqbrown.spam@gmail.com>
2026-04-16 04:14:17 +0000

    set for production, remove html from shuyuan template

--- app.py
+++ app.py
@@ -33,6 +33,5 @@ def index():
             'index.html'
             )
 
-
 if __name__ == '__main__': 
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
